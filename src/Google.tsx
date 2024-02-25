3fb4f323b10821c52ed6168e4372e5c4fbcb0777
SHIVA-6699 <pillashivakrishna6@gmail.com>
2024-02-25 22:25:13 +0530

    Loading Animation with sign in

--- src/Google.tsx
+++ src/Google.tsx
@@ -1,23 +1,45 @@
 import { signInWithPopup } from "firebase/auth";
-import { auth,provider } from "./config";
+import { auth, provider } from "./config";
+import logo from "./assets/google.jpg";
 import App from "./App";
 import { useEffect, useState } from "react";
 const Google = () => {
-    const [value, Setvalue] = useState<string | null>("")
-    function handlesubmit() {
-        signInWithPopup(auth, provider).then((data) => {
-            Setvalue(data.user.email ?? "")
-            localStorage.setItem("email", data.user.email ?? "")
-        })
+  const [value, Setvalue] = useState<string | null>("");
+  const [loading, SetLoading] = useState<boolean>(false);
+ async function handlesubmit() {
+    SetLoading(true);
+    try {
+        const data = await signInWithPopup(auth, provider);
+        Setvalue(data.user.email ?? "");
+        localStorage.setItem("email", data.user.email ?? "");
+    } catch (e) {
+        console.log(e);
+        // Handle error
+    } finally {
+        SetLoading(false);
     }
-    useEffect(()=>{
-        Setvalue(localStorage.getItem("email"))
-    },[])
-    return <div>
-        {value ? <App/> : 
-        <button onClick={() => handlesubmit()}>sign with google</button>
+}
+  useEffect(() => {
+    Setvalue(localStorage.getItem("email"));
+  }, []);
+
+  return (
+    <div>
+      {value ? (
+        <App />
+      ) : (
+        <button onClick={() => handlesubmit()} className="button-container">
+          <img src={logo} alt="" width={230} height={50} className="img" />
+          {loading && 
+          <span
+          className="spinner-border"
+          role="status"
+          aria-hidden="true"
+          ></span>
         }
-        
-    </div>;
+        </button>
+      )}
+    </div>
+  );
 };
 export default Google;
